be2005cbe9f46294b53443fad770fb60c3e13186
Kacper Gajdarski <gajdarski.kacper@gmail.com>
2025-06-09 14:16:25 +0100

    Refactor next.config.js to use ES module syntax for imports and exports

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-require("./src/env.js");
+import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {};
 
-module.exports = config;
+export default config;
