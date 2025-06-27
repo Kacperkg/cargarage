718d0ff7a5e0dadd8e18a43e4b52141c8d60e7be
Kacper Gajdarski <gajdarski.kacper@gmail.com>
2025-06-27 22:11:29 +0100

    Dashboard link works

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -1,3 +1,45 @@
+"use client";
+
+import MetricCardRow from "../_components/dashboard/metrics/metric-row";
+import QuickActions from "../_components/dashboard/quick-actions/quick-action";
+import FeaturedCards from "../_components/dashboard/featured-cards";
+import { useUser } from "@clerk/nextjs";
+import { api } from "~/trpc/react";
+import { useEffect } from "react";
+import { toast } from "sonner";
+
 export default function DashboardPage() {
-  return <div>TEST DOES THIS WORK</div>;
+  const { isSignedIn, user, isLoaded } = useUser();
+  const createUserMutation = api.createUser.createUser.useMutation();
+
+  useEffect(() => {
+    if (
+      isSignedIn &&
+      isLoaded &&
+      user &&
+      !createUserMutation.isPending &&
+      !createUserMutation.isSuccess &&
+      !createUserMutation.isError
+    ) {
+      createUserMutation.mutate(undefined, {
+        onError: () => {
+          toast.error("Error syncing user", {
+            description: "Error syncing user, please try again.",
+          });
+        },
+      });
+    }
+  }, [isSignedIn, isLoaded, user, createUserMutation]);
+
+  return (
+    <div className="flex h-screen w-full flex-col">
+      <div className="flex w-full flex-col gap-4 px-2 sm:px-4 md:mx-auto lg:px-16">
+        <MetricCardRow />
+        <div className="flex flex-col justify-between gap-4 xl:flex-row">
+          <FeaturedCards />
+          <QuickActions />
+        </div>
+      </div>
+    </div>
+  );
 }
