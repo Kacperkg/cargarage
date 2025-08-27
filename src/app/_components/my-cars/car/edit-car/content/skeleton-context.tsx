77a434f5c3ac1c06bf265f594503555dbbe850ca
Kacper Gajdarski <gajdarski.kacper@gmail.com>
2025-08-27 14:23:02 +0100

    .

--- src/app/_components/my-cars/car/edit-car/content/skeleton-context.tsx
+++ src/app/_components/my-cars/car/edit-car/content/skeleton-context.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ImageShowcase from "../image/image-show-case";
 
 import FormAction from "../form-action/form-action";
 
@@ -10,7 +9,6 @@ import OwnerShipSkeleton from "../ownership/ownership-skel";
 export default function SkeletonContent() {
   return (
     <div className="mt-8 flex h-screen w-full flex-col gap-4 px-2 sm:px-4 md:mx-auto lg:px-16">
-      <ImageShowcase />
       <BasicSkeleton />
       <PerformanceSkeleton />
       <OwnerShipSkeleton />
